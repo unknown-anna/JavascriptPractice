5c092e852325fdb631a24ea31d0a545b81279a98
unknown-anna <unknown2011.04.art@gmail.com>
2017-02-17 13:59:09 -0800

    more funny function

--- 02/clock.js
+++ 02/clock.js
@@ -739,11 +739,53 @@ function clock(){
 		}
 	}
 
+
+	//audios
 	var se_deta = new Audio('se/deta.mp3');
 	var se_ah = new Audio('se/ah.mp3');
 	var se_haxa = new Audio('se/haxa.mp3');
 	var se_yukkuri = new Audio('se/yukkuri.mp3');
 	var se_lunch_time = new Audio('se/lunch_time.mp3');
+	var se_pi = new Audio('se/pi.mp3');
+	var se_MGS = new Audio('se/MGS.mp3');
+	var se_conan = new Audio('se/conan.mp3');
+	var se_zelda = new Audio('se/zelda.mp3');
+	var se_mario = new Audio('se/mario.mp3');
+	var se_ff = new Audio('se/ff.mp3');
+
+
+	//fanbox
+	var fanbox = document.getElementById('funbox');
+	var arr_funbox_item = {
+		'pi'     : arr_item = [se_pi, ''],
+		'ah'     : arr_item = [se_ah, ''],
+		'deta'   : arr_item = [se_deta,'<img src="img/rainbow.gif"> ']
+	}
+
+	var arr_funbox_random_item = new Array(
+		arr_item = [se_yukkuri,'<img src="img/yukkuri.jpg">'],
+		arr_item = [se_MGS,    '<img src="img/MGS.jpg">'],
+		arr_item = [se_conan,  '<img src="img/conan.jpg">'],
+		arr_item = [se_zelda,  '<img src="img/zelda.jpg">'],
+		arr_item = [se_mario,  '<img src="img/mario.png">'],
+		arr_item = [se_ff,     '<img src="img/ff.jpg">']
+	);
+	
+	
+	function random_funbox(num){
+		
+
+		for(i = 0; i <= num; i++) {
+			console.log(num + " : i " + i);
+
+			if(i == num) {
+
+				arr_funbox_random_item[i][0].play();
+				funbox.innerHTML = arr_funbox_random_item[i][1];
+				break;
+			}
+		}
+	}
 
 	var i = setInterval(function(){
 	    // do your thing
@@ -753,7 +795,13 @@ function clock(){
 	    	dots_off();
 	    }
 	    
-	    date = new Date;
+	    date = new Date();
+
+	    // date.setHours(date.getHours() - 2 );
+	    // date.setMinutes(date.getMinutes() + 8);
+	    // date.setSeconds(30);
+
+
 	    hh = date.getHours();
 		mm = date.getMinutes();
 		ss = date.getSeconds();
@@ -768,15 +816,40 @@ function clock(){
 		s_1 = Math.floor(ss/10);
 		s_2 = ss - s_1*10;
 
-		if(s_1 == 0 && s_2 == 0) {
-			se_yukkuri.play();
+
+		
+		if(    h_1 == 1 && h_2 == 1
+			&& m_1 == 5 && m_2 == 9
+			&& s_1 == 5 && s_2 > 6) {
+			se_pi.play();
+
 		}
 
 		if(    h_1 == 1 && h_2 == 2
 			&& m_1 == 0 && m_2 == 0
 			&& s_1 == 0 && s_2 == 0) {
-			se_deta.play();
+			arr_funbox_item.deta[0].play();
+			fanbox.innerHTML = arr_funbox_item.deta[1];
 		}
+
+		if(    h_1 != 0 && h_2 != 0
+			&& m_1 != 0 
+			&& s_1 == 0 && s_2 == 0) {
+
+			console.log('test');
+			random_funbox(Math.floor((Math.random() * 6) + 1) -1);
+			// random_funbox(1);
+		}
+		
+		if(s_2 > 3 ) {
+			fanbox.innerHTML = '';
+		}
+
+		if(    m_1 == 3 && m_2 == 0
+			&& s_1 == 0 && s_2 == 0){
+			se_ah.play();
+		}
+
 
 	    clock(h_1, "h_1");
 	    clock(h_2, "h_2");
